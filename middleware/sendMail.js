debc53465332eb5f00d686cb02b7bb0fcaf7dbc5
varungowda07 <varungowdasr29@gmail.com>
2025-10-16 17:05:04 +0530

    modified the sendMail.js

--- middleware/sendMail.js
+++ middleware/sendMail.js
@@ -1,11 +1,13 @@
-const nodeMailer = require('nodemailer')
+const nodeMailer = require('nodemailer');
 
 const transport = nodeMailer.createTransport({
-    service:'gmail',
-    auth:{
-        user:process.env.EMAIL_ADDRESS,
-        pass:process.env.PASSWORD
-    }
-})
+    host: "smtp.gmail.com",
+    port: 465,      // SSL port
+    secure: true,   // true for 465, false for 587
+    auth: {
+        user: process.env.EMAIL_ADDRESS,
+        pass: process.env.PASSWORD, // App password
+    },
+});
 
-module.exports = transport
+module.exports = transport;
